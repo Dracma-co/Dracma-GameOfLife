eef774cb0853cc6a80079766ef891f4ad40a824d
Florencia Rocca <florenciarocca@live.com>
2020-12-21 19:40:12 -0300

    xdxdlol2

--- main.cpp
+++ main.cpp
@@ -4,41 +4,49 @@
 #define WIDTH 50
 #define HEIGHT 50
 
-char display[WIDTH][HEIGHT];
-int content[WIDTH][HEIGHT];
+typedef struct material {
+    int estado;
+    void (*instruccion[MAX_INSTRUCCIONES]) (struct material[MAX_TABLERO][MAX_TABLERO], int x, int y);
+} material_t;
 
-void paint() {
+void paint(material content[HEIGHT][WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            if (content[i][j] == 0)
-                printf("_|");
+            if (content[i][j].estado == 0)
+                printf("  ");
             else
-                printf("0|");
+                printf("||");
         }
         printf("\n");
     }
 }
 
-void TakeInput() {
+void take_input(int content[HEIGHT][WIDTH]) {
     content[25][10] = 1;
     content[26][10] = 1;
     content[27][10] = 1;
     content[28][10] = 1;
     content[29][10] = 1;
+    content[27][8] = 1;
+    content[27][9] = 1;
+    content[27][11] = 1;
+    content[27][12] = 1;
+}
+
+void inicializar(material content[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++)
+        for (int j = 0; j < WIDTH; j++)
+            content[i][j].estado = 0;
+    take_input(content);
+    paint(content);
 }
 
 bool play() {
     return false;
 }
 
-
-typedef struct material {
-    int estado;
-    void (*instruccion[MAX_INSTRUCCIONES]) (struct material[MAX_TABLERO][MAX_TABLERO], int x, int y);
-} material_t;
-
 int main(int argc, char* argv[]) {
-    TakeInput();
-    paint();
+    material content[WIDTH][HEIGHT];
+    inicializar(content);
     return 0;
 }
