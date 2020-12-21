707b9d79a0f6a810d32941c84545e2d334d096af
Juan <jbiancuzzo@fi.uba.ar>
2020-12-21 19:16:55 -0300

    Matriz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#define MAX_INSTRUCCIONES 10
+#define MAX_TABLERO 100
 
 using namespace std;
+
+typedef struct material {
+    int estado;
+    void (*instruccion[MAX_INSTRUCCIONES]) (struct material[MAX_TABLERO][MAX_TABLERO], int x, int y);
+} material_t;
 
 int main(int argc, char* argv[]) {
 
