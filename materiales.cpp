a1a9395b579b0b770c37fe9a6e376f2e9cd83cf4
Florencia Rocca <florenciarocca@live.com>
2020-12-22 12:02:45 -0300

    gah

---
+++ materiales.cpp
@@ -0,0 +1,28 @@
+#include "materiales.h"
+
+int material::num_neighbors(material* content[WIDTH][HEIGHT], int x, int y) {
+    int num = 0, pos_x, pos_y;
+
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            pos_x = (x + i < 0) ? WIDTH - 1 : (x + i) % WIDTH;
+            pos_y = (y + j < 0) ? HEIGHT - 1 : (y + j) % HEIGHT;
+            material* p1 = dynamic_cast<vivo*> (content[pos_x][pos_y]);
+            if (pos_x != x || pos_y != y)
+                if (p1)
+                    num++;
+        }
+    }
+    return num;
+}
+
+void vivo::reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y) {
+    int neighbors_alive = num_neighbors(current, x, y);
+    update[x][y] = (neighbors_alive == 3) ? new muerto() : current[x][y];
+}
+
+void muerto::reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y) {
+    int neighbors_alive = num_neighbors(current, x, y);
+    update[x][y] = ((neighbors_alive < 2 || neighbors_alive > 3)) ? new vivo() : current[x][y];
+}
+
--- materiales.h
+++ materiales.h
@@ -1,23 +1,38 @@
 #pragma once
-#include "material.h"
+#define MAX_MATERIAL 10
+#define WIDTH 50
+#define HEIGHT 50
+#define ALIVE 1
+#define DEATH 0
+
+class material
+{
+protected:
+    int estado;
+    int nombre;
+public:
+    material(int state, int name) : estado(state), nombre(name) {
+    };
+    material() : estado(0), nombre(0) {
+    }
+    virtual ~material() {}
+    int getEstado() { return estado; }
+    virtual void reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y) {};
+    int num_neighbors(material* content[WIDTH][HEIGHT], int x, int y);
+};
+
 class vivo : public material
 {
 public:
-	vivo() : material(1, 1) {};
-	~vivo() {}
-	void reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y) {
-		int neighbors_alive = num_neighbors(current, x, y);
-		update[x][y] = ((neighbors_alive < 2 || neighbors_alive > 3)) ? new muerto() : current[x][y];
-	}
+    vivo() : material(1, 1) {};
+    ~vivo() {}
+    void reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y);
 };
 
 class muerto : public material
 {
 public:
-	muerto() : material(0, 0) {};
-	~muerto() {}
-	void reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y) {
-		int neighbors_alive = num_neighbors(current, x, y);
-		update[x][y] = ((neighbors_alive < 2 || neighbors_alive > 3)) ? new vivo() : current[x][y];
-	}
+    muerto() : material(0, 0) {};
+    ~muerto() {}
+    void reaccionar(material* current[WIDTH][HEIGHT], material* update[WIDTH][HEIGHT], int x, int y);
 };
